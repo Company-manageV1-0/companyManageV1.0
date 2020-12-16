1c0b6da08a18ab61679e535f455d692b422bd512
sun-wan <2036314200@qq.com>
2020-12-16 23:15:02 +0800

    增加了登录前首页的路由

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Vuex from 'vuex'
 
 const Login = () => import('../views/Login.vue')
 const register = () => import('../views/register.vue')
+// const first_first = () => import('../views/first_first.vue')
 const first_first = () => import('../views/first_first.vue')
 
 const first = () => import('../views/user/first.vue')
